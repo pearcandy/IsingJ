0f6276386dfc0dc0889bd797c71ea5292169bf18
yassan1980 <y.nishi1980@gmail.com>
2019-07-05 16:11:46 +0900

    modify Ising.jl

--- src/IsingJ.jl
+++ src/IsingJ.jl
@@ -21,7 +21,7 @@ module IsingJ
 
     # main
     function main()
-        parser= ArgParseSettings("Julia code for solving 2D Ising model")
+        parser= ArgParseSettings("Julia code for solving 2D Ising model") # parser
         @add_arg_table parser begin
             "--lower_temp", "-l"
             arg_type = Float64
@@ -56,8 +56,7 @@ module IsingJ
             default  = 64
             help     = "simulation size"
         end
-            args = parse_args(parser)
-        
+            args = parse_args(parser)        
       
     """ Arguments        
         N_site    : number of site
@@ -68,6 +67,7 @@ module IsingJ
         inum_t    : number of temperature steps
         sweep_num : sweep number
         warm_up   : number of "warming-up" iteration  """
+
         N_site    = args["site_size"]
         j0_val    = args["exch_j"]
         low_t     = args["lower_temp"]
@@ -77,17 +77,17 @@ module IsingJ
         sweep_num = args["sweep_num"]
         warm_up   = args["warm_up"]
         cnst      = 2.26918  # Tc for square lattice 
+
         # initialization of the physical quantities
         spin      = zeros(N_site,N_site)
         mag       = 0
         mag_ave   = 0
         dt        = (up_t-low_t)/float(inum_t)
         _mag      = []
-        # initialization of the spin config
+
         Random.seed!(100)
-        spin_ini = cold_start(N_site)  # random_start(N_site) 
-        # start motecalro simulation
-        td=Dates.today()
+        spin_ini = cold_start(N_site)  # Initialization, random_start(N_site) 
+        td=Dates.today()               # start motecalro simulation
         println(" ")
         println(td)
         println(" ")
@@ -100,11 +100,9 @@ module IsingJ
         println("---------------------------------------")        
         open("log.dat","w") do io
             println(io,"#temperature magnetization energy Cv Chi Tc")
-            for i in 1:inum_t
+            for i in 1:inum_t       # main routine (montecalro sampling) 
                 _temp = low_t + dt*float(i)
-                # main routine (montecalro sampling) 
                 spin,mag,eng,cv,chi = mcsampling(spin_ini,sweep_num,warm_up,N_site,j0_val,ext_val,_temp)
-                # output format
                 @printf(io, " %+06f"  ,_temp)
                 @printf(io, " %+06f"  , mag)
                 @printf(io, " %+06f"  , eng)
@@ -112,15 +110,15 @@ module IsingJ
                 @printf(io, " %+06f"  , chi)
                 @printf(io, " %+06f\n", cnst)
                 append!(_mag,abs(mag))
-                # log
-                if i%10==0
+
+                if i%10==0          # log 
                     println(i,"/",inum_t,"steps")
                 else
                 end
             end
-        end        
-        # smoothing T vs magnetization
-        output_T_vs_mag(low_t,dt,_mag)
+        end
+        
+        output_T_vs_mag(low_t,dt,_mag)   # smoothing T vs magnetization
                 #-- plot (using python) --
         #    pyplot=pyimport("matplotlib.pyplot")
         #    numpy=pyimport("numpy")
